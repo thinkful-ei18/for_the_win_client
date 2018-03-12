3735fe0e4384778c232738678816acd95be5d49b
alishaevn <alieva.inc@gmail.com>
2018-03-12 10:57:47 -0700

    basic outlines

---
+++ src/components/TheDraft.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './styles/theDraft.css';
+
+
+export default class TheDraft extends Component {
+
+  render() {
+
+    return (
+      <div className='theDraft'>
+
+      </div>
+    );
+  }
+}
