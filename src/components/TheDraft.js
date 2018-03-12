2cc5703a69ae35be5af2715e76f4e64bfb47dff1
alishaevn <alieva.inc@gmail.com>
2018-03-12 12:41:27 -0700

    installed redux. created an action to pull api players.

--- src/components/TheDraft.js
+++ src/components/TheDraft.js
@@ -5,10 +5,14 @@ import './styles/theDraft.css';
 
 export default class TheDraft extends Component {
 
+  // when this component loads, dispatch the action that fetches all players from the api
+  // the players should each get their own li
+  
   render() {
 
     return (
       <div className='theDraft'>
+        <h1>Draft your team!</h1>
 
       </div>
     );
