a8057daa48e9845d434eaffe4972a18d29cd2589
alishaevn <alieva.inc@gmail.com>
2018-03-12 13:18:16 -0700

    installed redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import { draftReducer } from './reducers/draft.reducers';
 
-export default createStore(draftReducer);
+export default createStore(
+  draftReducer,
+  applyMiddleware(thunk)
+);
