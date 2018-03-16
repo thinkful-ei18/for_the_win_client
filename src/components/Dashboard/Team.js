6c815c3fca2d132e1cf2bbc4f9bed1627df626e4
alishaevn <alieva.inc@gmail.com>
2018-03-15 22:46:07 -0700

    also pull the date of the last game

--- src/components/Dashboard/Team.js
+++ src/components/Dashboard/Team.js
@@ -17,10 +17,6 @@ export class Team extends Component {
   }
 
   render() {
-    console.log('COMP TEAM: ', this.props.team);
-    console.log('TEAM STATS: ', this.props.stats)
-
-
     // const myTeam = this.props.team.map((player, index) => (
     //     <p key={index} className='rosterPlayer'>{player.firstName} {player.lastName}</p>
     // ));
@@ -28,6 +24,7 @@ export class Team extends Component {
     const playerStats = this.props.stats.map((player, index) => (
       <ul className='playerStats' key ={index}>
         <li className='playerName'>{player.firstName} {player.lastName}</li>
+        <li className='gameDay'>{player.dateOfGame}</li>
         <li className='offense'>{player.twoPointers}</li>
         <li className='offense'>{player.threePointers}</li>
         <li className='offense'>{player.freeThrows}</li>
@@ -43,6 +40,18 @@ export class Team extends Component {
       <div className='team'>
         <h1>Team</h1>
         <div>
+          <ul className='descriptions'>
+            <li className='title'>Player Name</li>
+            <li className='title'>Last Game:</li>
+            <li className='title'>2 PTS</li>
+            <li className='title'>3 PTS</li>
+            <li className='title'>FT</li>
+            <li className='title'>AST</li>
+            <li className='title'>REB</li>
+            <li className='title'>STL</li>
+            <li className='title'>BLK</li>
+            <li className='title'>PTS</li>
+          </ul>
           {playerStats}
         </div>
       </div>
