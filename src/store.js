bf89741de1f63f59f8002a067c0cc2e430c9f886
alishaevn <alieva.inc@gmail.com>
2018-03-16 08:06:35 -0700

    install/import redux-form

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { reducer as loginReducer } from 'redux-form';
 import thunk from 'redux-thunk';
 
 import { draftReducer } from './reducers/draft.reducers';
@@ -6,6 +7,7 @@ import { statsReducer } from './reducers/statsReducer';
 import { teamReducer } from './reducers/team.reducers';
 
 const rootReducer = combineReducers({
+  loginReducer,
   draftReducer,
   statsReducer,
   teamReducer
