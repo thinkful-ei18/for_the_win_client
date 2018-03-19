59c2131ba1a207d3aee4f4e1069b645642f92554
alishaevn <alieva.inc@gmail.com>
2018-03-19 16:03:24 -0700

    authToken is persisted when the draft page is refreshed

--- src/components/Draft/TheDraft.js
+++ src/components/Draft/TheDraft.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 import Players from './Players';
+import { checkUserAuth } from '../../actions/userActions';
 
 import './theDraft.css';
 
 
 export default class TheDraft extends Component {
   
+  componentWillMount() {
+    this.props.dispatch(checkUserAuth())
+  }
+
   render() {
 
     return (
