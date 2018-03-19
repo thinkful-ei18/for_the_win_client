fd21fcc4a7b29dc9d589c401342c24f4fcd3154e
alishaevn <alieva.inc@gmail.com>
2018-03-19 13:59:21 -0700

    updated env variables

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_DB_BASE_URL || 'http://localhost:8080'
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080'
 
 export const API_ACTIVE_PLAYERS_BASE_URL = process.env.REACT_APP_API_ACTIVE_PLAYERS_BASE_URL || 'http://localhost:8080/api'
 
