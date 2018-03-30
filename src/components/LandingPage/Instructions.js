798fca6fb7b90433cea6a7285e5b774796410dd9
alishaevn <alieva.inc@gmail.com>
2018-03-30 10:00:20 -0700

    updated instructions and styling

--- src/components/LandingPage/Instructions.js
+++ src/components/LandingPage/Instructions.js
@@ -9,10 +9,10 @@ export default class LandingPage extends Component {
 
     return (
       <div className='instructionsDiv'>
-        <h3 className='instructionsHeader'>Welcome to the #1 basketball stat tracker app!</h3>
+        <h3 className='instructionsHeader'>Welcome basketball fans!</h3>
         <p className='instructions'>1. Create a free user account</p>
         <p className='instructions'>2. Choose 10 NBA players whose stats you want to keep up with.</p>
-        <p className='instructions'>3. View each player's stats daily in one convenient location!</p>
+        <p className='instructions'>3. View your player's stats and plan which games to watch daily in one convenient location!</p>
       </div>
     );
   }
