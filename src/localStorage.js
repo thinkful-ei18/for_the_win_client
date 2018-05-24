819f92e0023505022ae7997a0ef98d967590507c
alishaevn <alieva.inc@gmail.com>
2018-05-24 15:01:18 -0700

    saving league name in local storage

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,4 +1,4 @@
-
+/* ========================= AUTH TOKEN ========================= */
 export const findAuthToken = () => {
   return localStorage.getItem('authToken');
 };
@@ -6,16 +6,35 @@ export const findAuthToken = () => {
 export const saveAuthToken = authToken => {
   try {
     localStorage.setItem('authToken', authToken);
-  } catch (e) { }
+  } 
+  catch (e) { }
 };
 
 export const deleteAuthToken = () => {
   try {
     localStorage.removeItem('authToken');
-  } catch (e) { }
+  } 
+  catch (e) { }
 };
 
-/**
+
+/* ========================= LEAGUE NAME ========================= */
+export const findLeagueName = () => {
+  try {
+    return localStorage.getItem('leagueName')
+  }
+  catch (e) {}
+}
+
+export const saveLeagueName = leagueName => {
+  try {
+    localStorage.setItem('leagueName', leagueName);
+  }
+  catch (e) {}
+}
+
+
+/*
  Resources:
- https://auth0.com/docs/security/store-tokens
+  - https://auth0.com/docs/security/store-tokens
  */
