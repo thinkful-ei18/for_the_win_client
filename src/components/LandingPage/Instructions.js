762947ce5a57df9c1b8f3636f369258b62258b9a
alishaevn <alieva.inc@gmail.com>
2018-05-26 15:56:29 -0700

    added info about the score calculation

--- src/components/LandingPage/Instructions.js
+++ src/components/LandingPage/Instructions.js
@@ -19,6 +19,7 @@ export default class LandingPage extends Component {
           <p className='instructions'>2. Create or join a league (up to 5 members).</p>
           <p className='instructions'>3. Form your team of 10 NBA players & follow their stats.</p>
           <p className='instructions'>4. Watch as the lead changes in your league throughout the season!</p>
+          <p className='instructions'>( Each offensive & defensive stat are valued at 1 point each. i.e., if a player had (4) 2 point shots, (1) assist and (3) rebounds in their last game, their total would be 8 points. )</p>
         </div>
       </div>
     );
