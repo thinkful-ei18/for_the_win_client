893b39b943b458be1592f8626f755a14e0726bc7
alishaevn <alieva.inc@gmail.com>
2018-10-25 11:01:54 -0700

    pulled out the input moment date/time picker to it's own file

---
+++ src/components/League/InputMomentPicker.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import InputMoment from 'input-moment';
+
+import './inputMomentPicker.css';
+
+export default class InputMomentPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      m: moment()
+    }
+  }
+
+  handleChange = m => {
+    this.setState({ m })
+  };
+
+  handleSave = () => {
+    console.log('saved', this.props.m.format('llll'));
+  };  
+
+  render() {
+
+    return(
+      <form>
+          <div className="input">
+            <input type="text" value={this.state.m.format('llll')} readOnly />
+          </div>
+          <InputMoment
+            moment={this.state.m}
+            onChange={this.handleChange}
+            minStep={5}
+            onSave={this.handleSave}
+          />
+      </form>
+    )
+  }
+
+};
