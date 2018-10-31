29b917c91ceecbdb54bf473bb7271b78aba227b0
alishaevn <alieva.inc@gmail.com>
2018-10-30 22:39:51 -0700

    conditional rendering of the dashboard components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,10 @@ import Header from './Header';
 import Team from './Team';
 import Score from './Score';
 import InputMomentPicker from './InputMomentPicker';
+import CountdownTimer from './CountdownTimer';
 import { checkUserAuth, logout } from '../actions/userActions';
+import { retrieveLeagues } from '../actions/leagueActions';
+import { findUsersLeague } from '../localStorage';
 
 import '../styles/dashboard.css';
 import '../styles/navbar.css';
@@ -16,13 +19,47 @@ export class Dashboard extends Component {
 
   componentWillMount() {
     this.props.dispatch(checkUserAuth());
+    this.props.dispatch(retrieveLeagues());
   }
 
   render() {
-    console.log('user:', this.props.user)
+    console.log('dash user:', this.props.user)
 
     if (this.props.loggedOut) {
       return <Redirect to='/' />
+    }
+
+    const usersDraftSchedule = (JSON.parse(findUsersLeague())).draftSchedule;
+    console.log('ULN:', usersDraftSchedule)
+    
+    // const usersLeague = this.props.leagues.filter( league => league.name === usersLeagueName);
+    // console.log('UL:', usersLeague)
+
+    let renderedDashboard;
+    // if (usersLeague.length && !usersLeague[0].draftSchedule) {
+    //   renderedDashboard = <InputMomentPicker />
+    // }
+    // else if(usersLeague.length && usersLeague[0].draftSchedule > new Date()){
+    //   // show the countdown
+    // }
+    // else {
+    //   renderedDashboard = <Team />
+    // }
+
+    // console.log('ds prop:', this.props.draftSchedule)
+    // console.log('ds prop string:', `${this.props.draftSchedule}`)
+    console.log('date:', new Date())
+    console.log('date string:', `${new Date()}`)
+
+    if (usersDraftSchedule === null) {
+      renderedDashboard = <InputMomentPicker />
+    }
+    else if(`${usersDraftSchedule}` < `${new Date()}`){
+      // show the countdown
+      renderedDashboard = <CountdownTimer />
+    }
+    else {
+      renderedDashboard = <Team />
     }
 
     const styles = {
@@ -60,9 +97,7 @@ export class Dashboard extends Component {
         </div>
 
         <Header />
-        <Score />
-        <InputMomentPicker />
-        <Team />
+        {renderedDashboard}
 
       </div>
     );
@@ -72,7 +107,9 @@ export class Dashboard extends Component {
 const mapStateToProps = state => ({
   authToken: state.userReducer.authToken,
   loggedOut: state.userReducer.loggedOut,
-  user: state.userReducer.user
+  user: state.userReducer.user,
+  leagues: state.leagueReducer.leagues !== null ? state.leagueReducer.leagues : [],
+  draftSchedule: state.leagueReducer.draftSchedule
 })
 
 export default connect(mapStateToProps)(Dashboard)
