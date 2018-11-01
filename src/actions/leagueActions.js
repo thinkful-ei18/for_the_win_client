09968426d1cb52e1c24110036fb036d8b8b782c2
alishaevn <alieva.inc@gmail.com>
2018-10-31 23:47:33 -0700

    removed console logs and unused code

--- src/actions/leagueActions.js
+++ src/actions/leagueActions.js
@@ -30,17 +30,6 @@ export const retrieveLeagues = () => (dispatch, getState) => {
   })
   .then(leagues => {
     dispatch(retrieveLeaguesSuccess(leagues));
-    console.log('LA LEAGUES:', leagues);
-
-    // let usersLeagueFromLocalStorage = JSON.parse(findUsersLeague());
-    // let usersLeagueName = usersLeagueFromLocalStorage.leagueName;
-    // console.log('LA ULN:', usersLeagueName);
-
-    // const usersLeague = leagues.filter(league => league.name === usersLeagueName)
-    // if(usersLeague[0].draftSchedule){
-
-    //   dispatch(saveUsersLeague(usersLeague[0].name, usersLeague[0].draftSchedule));
-    // }
 
     let usersLeagueName = (JSON.parse(findUsersLeague())).leagueName;
     console.log('LA ULN:', usersLeagueName);
@@ -109,7 +98,6 @@ export const createLeague = name => (dispatch, getState ) => {
 
     dispatch(createLeagueError(message))
   })
-
 }
 
 export const CREATE_LEAGUE_REQUEST = 'CREATE_LEAGUE_REQUEST';
@@ -127,6 +115,7 @@ export const createLeagueError = err => ({
   type: CREATE_LEAGUE_ERROR,
   err
 })
+
 
 
 /* ========================= JOIN A LEAGUE ========================= */
@@ -158,14 +147,8 @@ export const joinALeague = name => (dispatch, getState) => {
         }
       }
       else {
-      return res.json()
-      //   .then( league => {
-      //     saveLeagueName(league.name)
-      //   }
-      // )
-    }
-      
-      // return league;
+        return res.json()
+      }   
     })
     .then(league => {
       saveUsersLeague(league.name)
@@ -202,14 +185,10 @@ export const joinALeagueError = err => ({
 
 
 
-
 /* ========================= SET DRAFT SCHEDULE ========================= */
 export const setDraftSchedule = (name, draftSchedule) => (dispatch, getState) => {
   dispatch(setDraftScheduleRequest())
   const authToken = getState().userReducer.authToken;
-
-  console.log('LA SENT NAME:', name)
-  console.log('LA SENT SCH:', draftSchedule)
 
   fetch(`${API_BASE_URL}/league/schedule`, {
     method: 'POST',
@@ -240,35 +219,10 @@ export const setDraftSchedule = (name, draftSchedule) => (dispatch, getState) =>
     }
   })
   .then(leagueWithDraftSchedule => {
-    console.log({leagueWithDraftSchedule})
-    /*
-    {
-        "managers": [
-            {
-                "userId": "5bd8d5b3a7da35283eb0c1a0",
-                "username": "stephen30",
-                "team": "three-peat"
-            }
-        ],
-        "players": [],
-        "draftSchedule": "2018-11-01T06:15:00.000Z",
-        "name": "we them boys",
-        "id": "5bd8d674a7da35283eb0c1a1"
-    }
-    */
-   console.log('LA RETURNED NAME:', leagueWithDraftSchedule.name)
-   console.log('LA RETURNED SCH:', leagueWithDraftSchedule.draftSchedule)
-   
-  //  saveDraftScheduletoStateAndLocalStorage(leagueWithDraftSchedule.name, leagueWithDraftSchedule.draftSchedule, dispatch)
-  console.log('about to dispatch the draft schedule from helper function...')
-  saveUsersLeague(name, draftSchedule);
-  console.log('after saving to local storage');
-  dispatch(setDraftScheduleSuccess(draftSchedule));
-  console.log('after schedule action');
-   
+    saveUsersLeague(leagueWithDraftSchedule.name, leagueWithDraftSchedule.draftSchedule);
+    dispatch(setDraftScheduleSuccess(leagueWithDraftSchedule.draftSchedule));   
   })
   .catch(err => {
-    console.log('LA ERR:', err)
     let message = 'There was an error with the request.';
     
     dispatch(setDraftScheduleError(message));
